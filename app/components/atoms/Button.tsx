3b0bc45833a36f316700fec93d43974b0a9bb269
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-12 18:43:12 +0100

    almost done

--- app/components/atoms/Button.tsx
+++ app/components/atoms/Button.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 interface ButtonProps {
   label: string;
-  className: string;
+  className?: string;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
 }
 
 const Button: React.FC<ButtonProps> = ({label, onClick, type,className}) => {
   return (
-    <div className="">
+    <div className="w-fit h-fit p-2">
       <button type={type} className={className} onClick={onClick}>
         {label}
       </button>
--- app/components/organisms/udemy-business.tsx
+++ app/components/organisms/udemy-business.tsx
@@ -1,18 +1,50 @@
 import Image from "next/image";
 import React from "react";
+import Button from "../atoms/Button";
 
 type Props = {};
 
 const UdemyBusiness = (props: Props) => {
   return (
-    <div>
+    <div className="flex gap-8 w-fit py-20 mx-auto">
+      <div className="w-1/3 leading-loose ">
+        <div>
+          <Image
+            className=""
+            src="https://www.udemy.com/staticx/udemy/images/v7/logo-ub.svg"
+            alt="udemy business"
+            width={192}
+            height={32}
+          />
+          <h3 className="text-5xl font-bold">Upskill your team with Udemy Business</h3>
+        </div>
+        <div>
+          <ul>
+            <li>
+              Unlimited access to 22,000+ top Udemy courses, anytime, anywhere
+            </li>
+            <li>International course collection in 14 languages</li>
+            <li>Top certifications in tech and business</li>
+          </ul>
+        </div>
+        <div className="flex">
+          <Button
+            label="Get udeny Business"
+            className="hover:cursor-pointer text-white hover:opacity-70 bg-black p-3"
+          />
+          <Button
+            label="Learn more"
+            className="hover:cursor-pointer hover:bg-slate-100 border p-3"
+          />
+        </div>
+      </div>
       <div>
         <Image
-          className=""
-          src="https://www.udemy.com/staticx/udemy/images/v7/logo-ub.svg"
-          alt="udemy business"
-          width={50}
-          height={25}
+          className="bg-yellow"
+          src="/UB_Promo_800x800.jpeg"
+          alt="man and woman"
+          width={400}
+          height={400}     
         />
       </div>
     </div>
