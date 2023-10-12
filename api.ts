32206f404513386be50eb81fee46eba4b6f9059d
nsamedaisy <nsamedaisy@gmail.com>
2023-10-12 11:20:42 +0100

    added components and made changes with the register page

---
+++ api.ts
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+// export const fetchCourses = async (): Promise<Course[]> => {
+// 	try {
+// 		const response = await axios.get('/backend api endpoint')
+// 		return response.data
+// 	} catch (error) {
+// 		console.error('Error fetching courses:', error)
+// 		return [];
+// 	}
+// }
---
+++ app/components/pages/register.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import React, { FormEvent, useState } from 'react';
+// import axios from 'axios';
+// import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Input from '../atoms/input';
+import Button from '../atoms/button';
+
+
+type InitialStateProps = {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const initialState: InitialStateProps = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+function RegisterForm() {
+  const [state, setState] = useState(initialState);
+  // const router = useRouter();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    // axios
+    //   .post('/api/register', state)
+    //   .then(() => {
+    //     router.reload();
+    //   })
+    //   .then(() => {
+    //     setTimeout(() => {
+    //       router.push('/login');
+    //     }, 2500);
+    //   })
+    //   .catch((error: any) => {
+    //     throw new Error(error);
+    //   });
+  };
+
+  return (
+    <form className="text-center" onSubmit={onSubmit}>
+      <div className="flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2">
+        <Input
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={state.name}
+          onChange={handleChange}
+        />
+        <Input
+          id="email"
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={state.email}
+          onChange={handleChange}
+        />
+        <Input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={state.password}
+          onChange={handleChange}
+        />
+
+        <Button onClick={onSubmit} style={{ backgroundColor: 'black', color: 'white', fontWeight: 'bold', padding: '10px 4px' }} type="submit" text="Register" />
+
+      </div>
+
+      <div>
+        <div>
+          Do you have an account? <Link href="/login">Sign in</Link>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default RegisterForm;
