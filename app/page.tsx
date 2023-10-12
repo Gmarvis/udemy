4f2311a595bbb54082f96e50903e0b67f781037b
nsamedaisy <nsamedaisy@gmail.com>
2023-10-12 12:52:29 +0100

    resolving conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import Image from 'next/image';
-// import CategoryCards from './components/organisms/categoryCard';
+import CategoryCard from './components/organisms/CategoryCard'
 
-// const categories = [
-//   {
-//     id: 1,
-//     title: 'Web Development',
-//     image: 'web-development.jpg',
-//   },
-//   {
-//     id: 2,
-//     title: 'Data Science',
-//     image: 'data-science.jpg',
-//   },
-//   // Add more categories as needed
-// ];
+const categories = [
+  {
+    id: 1,
+    title: 'Web Development',
+    image: 'web-development.jpg',
+  },
+  {
+    id: 2,
+    title: 'Data Science',
+    image: 'data-science.jpg',
+  },
+  // Add more categories as needed
+];
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Udemy Clone</h1>
-      {/* <CategoryCards  categories={categories}/> */}
+      <CategoryCard categories={categories} />
     </main>
   );
 }
