515f4a34082e5a8dac92162b440411b60d619c1c
ricardo <ricardotsopse@gmail.com>
2023-10-12 16:40:46 +0100

    login page implementation

---
+++ app/components/organisms/login.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from '../atoms/Button'
+
+const Login = () => {
+  return (
+    <div>
+        <div>
+        <Button/>
+        <Button/>
+        <Button/>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Login
