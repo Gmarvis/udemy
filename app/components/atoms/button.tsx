844fc1692e7784fe3c717ca1596bed95e4d0a608
nsamedaisy <nsamedaisy@gmail.com>
2023-10-12 16:09:57 +0100

    fixed button component

--- app/components/atoms/button.tsx
+++ app/components/atoms/button.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+'use client'
 
 type ButtonProps = {
-    text: string;
-    onClick: () => void;
-    style?: React.CSSProperties;
-  };
+  label?: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  outline?: boolean;
+  small?: boolean
+  type: any
+  disabled?: boolean;
+};
 
-  const Button: React.FC<ButtonProps> = ({ text, onClick, style }) => {
-    return (
-      <button onClick={onClick} style={style}>
-        {text}
-      </button>
-    );
-  };
-  
-
-export default Button;
+export default function Button({ label, onClick, type, outline, small, disabled } :ButtonProps) {
+  return (
+    <button onClick={onClick} type={type}
+      className={`relative rounded-lg hover:opacity-80 transition ${outline ? 'bg-white' : 'bg-purple-500 w-full'} ${outline ? 'text-black' : 'text-white'} ${small ? 'py-1' : 'py-3'} ${small ? 'text-sm' : 'text-lg'} ${small ? 'border-[1px]' : 'border-2'}`} >
+      {label}
+    </button>
+  )
+}
