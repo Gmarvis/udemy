1d44b3f38093e74962190b078e5dc2b40965d183
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-12 05:55:26 +0100

    footer section done

--- app/components/organisms/footer.tsx
+++ app/components/organisms/footer.tsx
@@ -11,72 +11,101 @@ function Footer() {
           <strong className="text-purple-300 py-">Udemy Business</strong> to
           build in-demande career skills.
         </h1>
-        <div className="flex md:gap-8 ">
+        <div className="flex md:gap-8 gap-5 flex-wrap pt-3">
           <Image
             src="https://s.udemycdn.com/partner-logos/v4/nasdaq-light.svg"
             alt="Nasdaq"
-            className="w-20 p-0 md:w-24"
+            className="w-24 p-0 md:w-24"
             width={10}
             height={5}
           />
           <Image
             src="https://s.udemycdn.com/partner-logos/v4/volkswagen-light.svg"
             alt="volsk wargen"
-            className="w-10 md:w-20"
+            className="w-9 md:w-9"
             width={10}
             height={5}
           />
           <Image
             src="https://s.udemycdn.com/partner-logos/v4/box-light.svg"
             alt="box"
-            className="w-20"
+            className="w-16 md:w-16"
             width={10}
             height={5}
           />
           <Image
             src="https://s.udemycdn.com/partner-logos/v4/netapp-light.svg"
             alt="net app"
-            className="w-10"
+            className="md:w-28 w-28"
             width={10}
             height={5}
           />
           <Image
             src="https://s.udemycdn.com/partner-logos/v4/eventbrite-light.svg"
             alt="eventbright"
-            className="w-20"
+            className="w-28"
             width={10}
             height={5}
           />
         </div>
       </div>
       <hr />
-      <div className="px-4 pb-8">
+      <div className="px-4 pb-8 md:hidden">
         <div className="text-white hover:cursor-pointer flex gap-2 px-4 py-2 w-36 my-10 border">
           <TfiWorld className="my-auto "/>
           <p >English</p>
         </div>
         <div className="text-white md:flex md:gap-8 md:leading-8  md:pb-10">
-          <ul className="w-3/12  ">
+          <ul className="md:w-2/12 ">
             <li className="hover:underline hover:cursor-pointer">Udemy Business</li>
             <li className="hover:underline hover:cursor-pointer">Teach on Udemy</li>
             <li className="hover:underline hover:cursor-pointer">Get the app</li>
             <li className="hover:underline hover:cursor-pointer">About us</li>
             <li className="hover:underline hover:cursor-pointer">Contact us</li>
           </ul>
-          <ul className="w-3/12 ">
+          <ul className="md:w-2/12 ">
             <li className="hover:underline hover:cursor-pointer">Careers</li>
             <li className="hover:underline hover:cursor-pointer">Blog</li>
             <li className="hover:underline hover:cursor-pointer">Help and Support</li>
             <li className="hover:underline hover:cursor-pointer">Affilaite</li>
             <li className="hover:underline hover:cursor-pointer">Investors</li>
           </ul>
-          <ul className="w-4/12 ">
+          <ul className="md:w-2/12 ">
             <li className="hover:underline hover:cursor-pointer">Terms</li>
             <li className="hover:underline hover:cursor-pointer">Privacy policy</li>
             <li className="hover:underline hover:cursor-pointer">Cookies settings</li>
             <li className="hover:underline hover:cursor-pointer">Sitemap</li>
             <li className="hover:underline hover:cursor-pointer">Accessibility statement</li>
           </ul>
+        </div>
+      </div>
+      <div className="px-4 pb-8 hidden md:flex md:justify-between ">
+        <div className="text-white md:flex md:gap-8 md:leading-8 md:w-4/5 md:pt-6 md:pb-10">
+          <ul className="md:w-2/12 ">
+            <li className="hover:underline hover:cursor-pointer">Udemy Business</li>
+            <li className="hover:underline hover:cursor-pointer">Teach on Udemy</li>
+            <li className="hover:underline hover:cursor-pointer">Get the app</li>
+            <li className="hover:underline hover:cursor-pointer">About us</li>
+            <li className="hover:underline hover:cursor-pointer">Contact us</li>
+          </ul>
+          <ul className="md:w-2/12 ">
+            <li className="hover:underline hover:cursor-pointer">Careers</li>
+            <li className="hover:underline hover:cursor-pointer">Blog</li>
+            <li className="hover:underline hover:cursor-pointer">Help and Support</li>
+            <li className="hover:underline hover:cursor-pointer">Affilaite</li>
+            <li className="hover:underline hover:cursor-pointer">Investors</li>
+          </ul>
+          <ul className="md:w-2/12 ">
+            <li className="hover:underline hover:cursor-pointer">Terms</li>
+            <li className="hover:underline hover:cursor-pointer">Privacy policy</li>
+            <li className="hover:underline hover:cursor-pointer">Cookies settings</li>
+            <li className="hover:underline hover:cursor-pointer">Sitemap</li>
+            <li className="hover:underline hover:cursor-pointer">Accessibility statement</li>
+          </ul>
+        </div>
+        <div className="text-white hover:cursor-pointer flex h-fit gap-2 md:mt-6 px-4 py-2 w-36 my-10 border ">
+          <TfiWorld className="my-auto "/>
+          <p >English</p>
         </div>
       </div>
       <div className="px-4 md:flex md:justify-between">
