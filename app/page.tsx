7881ccbf7ec77d85f68067aac2d9ed085ee14d00
nsamedaisy <nsamedaisy@gmail.com>
2023-10-12 14:38:11 +0100

    working on the course category

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,10 @@
+'use client'
+
 import React from 'react';
-import Image from 'next/image';
-// import CategoryCard from './components/organisms/CategoryCard'
+import HomePage from './components/pages/homePage'
 
-// const categories = [
-//   {
-//     id: 1,
-//     title: 'Web Development',
-//     image: 'web-development.jpg',
-//   },
-//   {
-//     id: 2,
-//     title: 'Data Science',
-//     image: 'data-science.jpg',
-//   },
-//   // Add more categories as needed
-// ];
+const IndexPage: React.FC = () => {
+  return <HomePage />;
+};
 
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Udemy Clone</h1>
-      {/* <CategoryCard categories={categories} /> */}
-    </main>
-  );
-}
+export default IndexPage;
