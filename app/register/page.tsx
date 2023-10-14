a5e686170481010c354013d06fbbf920c1c27966
ricardo <ricardotsopse@gmail.com>
2023-10-14 01:47:02 +0100

    first commit for this branch

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputField from '../components/atoms/input'
+
+const registerPage = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default registerPage
