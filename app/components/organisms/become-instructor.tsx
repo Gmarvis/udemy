9af27409a9aa3088281014fc7d9c321d0c424fd3
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-16 14:31:18 +0100

    addimg some styles

--- app/components/organisms/become-instructor.tsx
+++ app/components/organisms/become-instructor.tsx
@@ -7,22 +7,28 @@ type Props = {};
 const BecomeInstructor = (props: Props) => {
   return (
     <div>
-      <div>
+      <div className="py-8 md:w-[50%] md:mx-auto md:justify-between md:bg-red-500 md:flex md:gap-8">
         <Image
-        className=" h-[350px] md:h-auto"
-        src="/herosec1.jpeg"
-        alt="become instructor"
-        loading="lazy"
-        width={600}
-        height={450}
+          className=" md:h-[350px] "
+          src="/herosec1.jpeg"
+          alt="become instructor"
+          loading="lazy"
+          width={600}
+          height={450}
         />
-        <h1 className=" ">Become an instructor</h1>
-        <p>Instructors from around the world teach millions of learners from 
-          Udemy. We provide the tool and skills to teach what you like.
-        </p>
-        <Button
-          label="Start teaching today"
-          className=" hover:cursor-pointer w-full text-white hover:opacity-70 bg-black p-2"/>
+        <div className="md:w-[50%] ">
+          <h1 className="text-2xl md:text-left py-4 font-semibold text-center ">
+            Become an instructor
+          </h1>
+          <p className="text-center md:text-left">
+            Instructors from around the world teach millions of learners from
+            Udemy. We provide the tool and skills to teach what you like.
+          </p>
+          <Button
+            label="Start teaching today"
+            className=" hover:cursor-pointer md:justify-self-start md:w-fit w-full text-white hover:opacity-70 bg-black p-2"
+          />
+        </div>
       </div>
     </div>
   );
