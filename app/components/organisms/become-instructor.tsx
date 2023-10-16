54b88df87b526662539f5b3fcf09cc32b16633ef
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-16 11:28:12 +0100

    first commit

--- app/components/organisms/become-instructor.tsx
+++ app/components/organisms/become-instructor.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Button from "../atoms/Button";
 
 type Props = {};
 
@@ -8,15 +9,20 @@ const BecomeInstructor = (props: Props) => {
     <div>
       <div>
         <Image
-        className=""
-        src="https://s.udemycdn.com/home/non-student-cta/instructor-mobile-v3.jpg"
+        className=" h-[350px] md:h-auto"
+        src="/herosec1.jpeg"
         alt="become instructor"
+        loading="lazy"
         width={600}
         height={450}
         />
-        <h2>Become an instructor</h2>
+        <h1 className=" ">Become an instructor</h1>
         <p>Instructors from around the world teach millions of learners from 
-          Udemy. We provide the tool and skills to teach what you like.</p>
+          Udemy. We provide the tool and skills to teach what you like.
+        </p>
+        <Button
+          label="Start teaching today"
+          className=" hover:cursor-pointer w-full text-white hover:opacity-70 bg-black p-2"/>
       </div>
     </div>
   );
