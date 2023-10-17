631e1906225f423aa2498ffd184aca0926bf2bb4
nsamedaisy <nsamedaisy@gmail.com>
2023-10-17 11:38:39 +0100

    fixing conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import Logos from "./components/molecules/logos";
-import SliderComponent from "./components/organisms/slide/Slider";
-import HeroSection from "./components/organisms/heroSection";
+import Logos from './components/molecules/logos'
+import HeroSection from './components/organisms/heroSection'
+import Testimonials from './components/organisms/testimonails'
 
 export default function Home({searchParams}:{searchParams: {['showDialog'] : string} }) {
  
   return (
     <main>
-      {/* <HeroSection /> */}
-      {/* <Logos /> */}
-      {/* <SliderComponent /> */}
+       <HeroSection/>  
+       <Logos/>
+       <Testimonials />
     </main>
   );
 }
