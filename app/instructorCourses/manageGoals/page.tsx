2ceeadb720bad4c6d389bc50937b63b0b0f92681
nsamedaisy <nsamedaisy@gmail.com>
2023-10-19 13:52:42 +0100

    working on the side check box of the pages

--- app/instructorCourses/manageGoals/page.tsx
+++ app/instructorCourses/manageGoals/page.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+'use client'
 
-type Props = {}
+import { useState } from 'react';
 
-export default function page({}: Props) {
+const checkboxes = [
+  {
+    id: 1,
+    name: 'Plan Your Course',
+    checks: ['Intended learners', 'Course structure', 'Setup & test video'],
+  },
+  {
+    id: 2,
+    name: 'Create Your Content',
+    checks: ['Film & edit', 'Curriculum', 'Captions (optional)', 'Accessibility'],
+  },
+  {
+    id: 3,
+    name: 'Publish Your Course',
+    checks: ['Course Landing Page', 'Pricing', 'Promotions', 'Course messages'],
+  },
+];
+
+const ManageGoals = () => {
+  const [activePage, setActivePage] = useState('');
+
+  const handleCheckboxClick = (page: string) => {
+    setActivePage(page);
+  };
+
   return (
-    <div>Instrutor Course manage Goals page
-        
+    <div>
+      <div className="flex flex-col space-y-2">
+        {checkboxes.map((checkbox) => (
+          <div key={checkbox.id}>
+            <h2>{checkbox.name}</h2>
+            {checkbox.checks.map((check, index) => (
+              <label className="flex items-center space-x-2" key={index}>
+                <input
+                  type="checkbox"
+                  checked={activePage === checkbox.name && activePage === check}
+                  onChange={() => handleCheckboxClick(check)}
+                />
+                <span>{check}</span>
+              </label>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-4">
+        {activePage && (
+          <h2>
+            {checkboxes.find((checkbox) => checkbox.checks.includes(activePage))?.name}
+          </h2>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default ManageGoals;
