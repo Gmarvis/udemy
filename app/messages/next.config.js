f7ff16fb5cc2230cfa080feca0220ff301b2ff36
gmarvis <sgmarvis@gmail.com>
2023-10-19 12:12:12 +0100

    implemented the backend connections

--- app/messages/next.config.js
+++ app/messages/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "Access-Control-Allow-Headers": "Content-Type,Authorization",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
