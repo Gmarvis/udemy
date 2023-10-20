8bf057a99d78c1f52feb289d5856856327b61853
nsamedaisy <nsamedaisy@gmail.com>
2023-10-20 17:04:28 +0100

    course structure page

--- app/components/organisms/categoryCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CategoryCard() {
-  return (
-    <div>CategoryCard</div>
-  )
-}
-
-export default CategoryCard
--- app/components/organisms/featuredInstructorsSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function FeaturedInstructorsSection() {
-  return (
-    <div>FeaturedInstructorsSection</div>
-  )
-}
-
-export default FeaturedInstructorsSection
