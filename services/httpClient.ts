ac6654fcf92ce38cc8a4213838303bf1b2b8c629
gmarvis <sgmarvis@gmail.com>
2023-10-23 15:19:15 +0100

    conflicts resolved

---
+++ services/httpClient.ts
@@ -0,0 +1,20 @@
+const Headers = {
+  "Content-Type": "application/json",
+};
+
+export default class ApiCall {
+  async GET(url: string, _headers: HeadersInit = {}) {
+    return fetch(url, {
+      method: "GET",
+      headers: { ...Headers, ..._headers },
+    }).then((res) => res.json());
+  }
+
+  async POST(url: string, body: any, _headers: HeadersInit = {}) {
+    return fetch(url, {
+      method: "POST",
+      headers: { ...Headers, ..._headers },
+      body: JSON.stringify(body),
+    }).then((res) => res.json());
+  }
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,16 @@
-import type { Config } from 'tailwindcss'
-import {nextui} from "@nextui-org/react";
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     fontFamily: {
-      "segoe": "Segoe UI Emoji"
+      segoe: "Segoe UI Emoji",
     },
     screens: {
       sm: "600px",
@@ -30,12 +30,12 @@ const config: Config = {
     },
     extend: {
       zIndex: {
-        "100":"100"
+        "100": "100",
       },
       listStyleImage: {
         checkmark: "checkmark-list-style",
         overlay: "modal-overlay",
-        modal: "popup-modal"
+        modal: "popup-modal",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -43,10 +43,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       spacing: {
-        "17":" 4.5rem",
+        "17": " 4.5rem",
         "23": "5.5rem",
         "90": "22rem",
-        "100": '25rem', 
+        "100": "25rem",
         "110": "30rem",
         "128": "32rem",
         "144": "36rem",
@@ -73,10 +73,10 @@ const config: Config = {
       gray: "#ccc",
       gray2: "#ddd",
       udemy: "#6A6F73",
+      errRed: "#fca5a5",
     },
   },
   darkMode: "class",
-  plugins: [nextui()]
+  plugins: [nextui()],
 };
-export default config
-
+export default config;
