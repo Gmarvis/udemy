274e035120bfea6852a2d06157b134d8f8843c59
gmarvis <sgmarvis@gmail.com>
2023-10-23 16:17:04 +0100

    uptimized build

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Providers> {children}</Providers>
-      </body>
+      <Providers>
+        <body className={inter.className}>{children}</body>
+      </Providers>
     </html>
   );
 }
