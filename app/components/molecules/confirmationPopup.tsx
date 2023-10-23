253b5d1702ce317df58ffde718edfddad17a5502
nsamedaisy <nsamedaisy@gmail.com>
2023-10-23 18:37:55 +0100

    import the confirm popup component on my instructor/course/manage/curriculum

--- app/components/molecules/confirmationPopup.tsx
+++ app/components/molecules/confirmationPopup.tsx
@@ -25,17 +25,17 @@ const ConfirmationPopup: React.FC<ConfirmationPopupProps> = ({ onCancel, onDelet
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div ref={popupRef} className="bg-white p-4">
-        <div className="flex justify-between">
+      <div ref={popupRef} className="bg-white px-8 py-6">
+        <div className="flex justify-between mb-5">
           <h2 className="text-xl font-bold">Please confirm</h2>
-          <BsX className="cursor-pointer" onClick={onCancel} />
+          <BsX className="cursor-pointer text-2xl" onClick={onCancel} />
         </div>
-        <p>Are you sure you want to delete this section?</p>
-        <div className="flex justify-end mt-4">
-          <button className="mr-2" onClick={onCancel}>
+        <p className='w-96'>You are about to remove a curriculum item. Are you sure you want to continue?</p>
+        <div className="flex justify-end mt-6">
+          <button className="mr-3" onClick={onCancel}>
             Cancel
           </button>
-          <button className='px-4 py-2 bg-black text-white font-bold' onClick={onDelete}>
+          <button className='px-5 py-2 bg-black text-white font-bold' onClick={onDelete}>
             Ok
           </button>
         </div>
