df34451ffd99bd1e35ce31be4a02dcb3fd7138e2
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-23 14:43:03 +0100

    fixing the colors on the footer and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,7 @@ import { Inter } from "next/font/google";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import MainNav from "./components/MainNav";
-import UdemyBusiness from "./components/organisms/udemy-business";
-import BecomeInstructor from "./components/organisms/become-instructor";
-import FeatureCategory from "./components/organisms/feature-category";
-import FooterLandingPage from "./components/organisms/footer-landingpage";
 import LogiNav from "./components/organisms/login-navbar";
-import Footer from "./components/organisms/footer-logout";
 import { Providers } from "./context/Provider";
 import React from "react";
 
