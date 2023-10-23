63790a0ce4e7181b317557d2cc6794dfa6075c20
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-23 13:22:26 +0100

    some changes

--- app/components/pages/login.tsx
+++ app/components/pages/login.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useState } from "react";
 import InputField from "../atoms/input";
@@ -10,55 +10,54 @@ import PasswordField from "../atoms/passwordField";
 // import { signIn } from 'next-auth/react'
 import { useRouter } from "next/navigation";
 
-const LoginForm: NextPage =  () => {
+const LoginForm: NextPage = () => {
   const router = useRouter();
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  // const formData = new FormData (e.currentTarget);
-  //  const response =await signIn('credentials', {
-  //   email: formData.get('email'),
-  //   password: formData.get('password'),
-  //   redirect: false,
-  //  }) ;
-  //  console.log(response);
-  //  if (!response?.error){
-  //   router.push("/");     
-  //  }
-     
+    // const formData = new FormData (e.currentTarget);
+    //  const response =await signIn('credentials', {
+    //   email: formData.get('email'),
+    //   password: formData.get('password'),
+    //   redirect: false,
+    //  }) ;
+    //  console.log(response);
+    //  if (!response?.error){
+    //   router.push("/");
+    //  }
   };
 
   return (
     <div className="flex flex-col items-center gap-2 justify-center mt-10 mb-8">
-      <form  onSubmit={handleSubmit} className="flex flex-col gap-2 w-[350px]">
-      <h3 className="text-[16px] font-bold">Log in to your Udemy account</h3>
-      <div className="flex flex-col w-[350px] gap-2">
-        <li className="border border-gray-950 flex items-center gap-4 font-bold px-4 py-2 h-[50px]">
-          <Image
-            src="/google.jpg"
-            alt="google_logo"
-            width={40}
-            height={40}
-            className="bg-transparent"
-          />
-          <h3>Continue with Google</h3>
-        </li>
-        <li className="border border-gray-950 flex items-center gap-4 font-bold px-4 py-2 h-[50px]">
-          <Image
-            src="/facebook.png"
-            alt="facebook_logo"
-            width={40}
-            height={40}
-          />
-          <h3>Continue with Facebook</h3>
-        </li>
-        <li className="border border-gray-950 flex items-center gap-4 font-bold px-4 py-2 h-[50px]">
-          <Image src="/apple.jpg" alt="apple_logo" width={35} height={30} />
-          <h3>Continue with Apple</h3>
-        </li>
-      </div>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-[350px]">
+        <h3 className="text-[16px] font-bold">Log in to your Udemy account</h3>
+        <div className="flex flex-col w-[350px] gap-2">
+          <li className="border border-gray-950 flex items-center gap-4 font-bold px-4 py-2 h-[50px]">
+            <Image
+              src="/google.jpg"
+              alt="google_logo"
+              width={40}
+              height={40}
+              className="bg-transparent"
+            />
+            <h3>Continue with Google</h3>
+          </li>
+          <li className="border border-gray-950 flex items-center gap-4 font-bold px-4 py-2 h-[50px]">
+            <Image
+              src="/facebook.png"
+              alt="facebook_logo"
+              width={40}
+              height={40}
+            />
+            <h3>Continue with Facebook</h3>
+          </li>
+          <li className="border border-gray-950 flex items-center gap-4 font-bold px-4 py-2 h-[50px]">
+            <Image src="/apple.jpg" alt="apple_logo" width={35} height={30} />
+            <h3>Continue with Apple</h3>
+          </li>
+        </div>
         <InputField
           name="email"
           type="text"
@@ -68,19 +67,26 @@ const LoginForm: NextPage =  () => {
           onChange={(e) => setEmail(e.target.value)}
         />
         <PasswordField
-          password={password} setPassword={setPassword}  
+          password={password}
+          setPassword={setPassword}
           className="w-full h-[60px] placeholder:text-black placeholder:font-bold text-[12px] px-4 pb-8"
           icon="eye"
           name="password"
         />
       </form>
       <div className="flex items-center justify-center bg-violet-600 w-[350px] h-[50px] my-2">
-        <Button className="text-white font-black text-[16px]" label="Log in" type={"submit"} />
+        <Button
+          className="text-white font-black text-[16px]"
+          label="Log in"
+          type={"submit"}
+        />
       </div>
       <h3>
         or {""}
         <Link legacyBehavior href="#">
-        <a className="text-violet-600 font-bold text-[14px]">Forgot Password?</a>
+          <a className="text-violet-600 font-bold text-[14px]">
+            Forgot Password?
+          </a>
         </Link>
       </h3>
 
@@ -88,8 +94,11 @@ const LoginForm: NextPage =  () => {
 
       <p className="text-[15px] text-center">
         Don&lsquo;t have an account?
-        <Link href="/register" legacyBehavior>
-           <a href="/register" className="text-violet-600 font-bold text-[14px]">Sign up <br /> Log in with your organisation </a>
+        <a href="/register" className="text-violet-600 font-bold text-[14px]">
+          Sign up 
+        </a>
+        <Link href="#" legacyBehavior>
+          <b>Log in with your organisation</b>
         </Link>
       </p>
     </div>
