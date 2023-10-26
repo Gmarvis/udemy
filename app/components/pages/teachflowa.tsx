d906d0471c97180f0e71a61a579ad21ab47cdbb7
ricardo <ricardotsopse@gmail.com>
2023-10-26 13:35:53 +0100

    some changes made

--- app/components/pages/teachflowa.tsx
+++ app/components/pages/teachflowa.tsx
@@ -9,17 +9,6 @@ import { useRouter } from "next/navigation";
 
 const Teachflowa = () => {
   const router = useRouter();
-  const [borderColor, setBorderColor] = useState("gray");
-  const [borderColora, setBorderColora] = useState('gray');
-  const [btnColor, SetBtnColor] = useState("gray")
-
-  const handleBoxa = () => {
-    setBorderColor("black");
-  };
-
-  const handleBoxb = () => {
-    setBorderColora("black");
-  };
 
   const handleClick = () => {
     router.push("/teachflowb");
@@ -58,16 +47,7 @@ const Teachflowa = () => {
           </h1>
         </div>
         <div className="flex gap-8 max-[550px]:flex-wrap max-[550px]:flex-row max-[550px]:items-center max-[550px]:ml-8">
-          <div
-            style={{
-              borderColor,
-              borderWidth: "3px",
-              borderStyle: "solid",
-              cursor: "pointer",
-            }}
-            onClick={handleBoxa}
-            className="w-[250px] h-[290px] border border-gray flex flex-col items-center justify-center gap-2 p-4 cursor-pointer hover:bg-gray "
-          >
+          <div className="w-[250px] h-[290px] border border-gray flex flex-col items-center justify-center gap-2 p-4 cursor-pointer hover:bg-gray ">
             <MdOndemandVideo size={30} />
             <h2 className="font-bold">Course</h2>
             <h3 className="text-[15px] text-center">
@@ -76,16 +56,7 @@ const Teachflowa = () => {
             </h3>
           </div>
 
-          <div
-            style={{
-              borderColora,
-              borderWidth: "3px",
-              borderStyle: "solid",
-              cursor: "pointer",
-            }}
-            onClick={handleBoxb}
-            className="w-[250px] h-[290px] border border-gray flex flex-col items-center justify-center gap-2 p-4 cursor-pointer hover:bg-gray"
-          >
+          <div className="w-[250px] h-[290px] border border-gray flex flex-col items-center justify-center gap-2 p-4 cursor-pointer hover:bg-gray">
             <FaRegListAlt size={27} />
             <h2 className="font-bold">Practical exercises</h2>
             <h3 className="text-[15px] text-center">
@@ -98,11 +69,6 @@ const Teachflowa = () => {
 
       <div className="border w-full absolute bottom-0 shadow shadow-gray">
         <Button
-        style={{
-          backgroundColor: btnColor,
-          borderWidth: '3px',
-          borderStyle: 'solid',
-        }}
           type="submit"
           className=" bg-gray px-6 py-[12px] my-2"
           label="Continue"
