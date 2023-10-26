58bf09eee9b63ad109380a91706d7eae7c9a5558
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-26 15:59:09 +0100

    done implementing a popup

--- app/components/molecules/my-list.tsx
+++ app/components/molecules/my-list.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { HiPencil } from "react-icons/hi";
 import { MdDelete } from "react-icons/md";
+import Popup from "./popup";
+import LogiNav from "../organisms/login-navbar";
 type Props = {};
 
 const MyList = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex gap-4 w-1/2 mx-auto">
-      <h1>
-        Instructor Collaborator
-      </h1>
-      <HiPencil/>
-      <MdDelete/>
+    <div>
+      <div className="flex gap-4 w-1/2 mx-auto">
+        <h1>Instructor Collaborator</h1>
+        <HiPencil onClick={() => setIsOpen((prev) => !prev)} />
+        <MdDelete />
+      </div>
+      {isOpen && (
+        <Popup
+          handleClose={() => setIsOpen((prev) => !prev)}
+        >
+          <LogiNav/>
+        </Popup>
+      )}
     </div>
   );
 };
---
+++ app/components/molecules/popup.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Props {
+  handleClose(): void
+  children: React.ReactElement
+};
+
+const Popup: React.FC<Props> = ({handleClose, children}: Props) => {
+  return (
+    <div className="fixed bg-black opacity-80 w-full h-full top-0 left-0">
+      <div className="w-[70%] relative bg-white opacity-20 rounded p-5 border overflow-auto shadow md:w-[40%] max-h-fit my-0 mx-auto mt-[calc[100vh_-_90vh_-_20px]] md:mt-[calc[100vh_-_85vh_-_20px]]">
+        <span onClick={handleClose} className="content-['x'] cursor-pointer text-center right-[calc[15%-30px]] fixed md:right-[calc[30%-30px]]  top-[calc[100vh-90vh-33px]] md:top-[calc[100vh-85vh-33px]] bg-gray w-5 h-5 text-red rounded-full leading-5 border text-xl">
+          x
+        </span>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
