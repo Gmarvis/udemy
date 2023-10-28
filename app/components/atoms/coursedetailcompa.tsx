8c34d44ed89e52db9b570018a98f567d963fdada
ricardo <ricardotsopse@gmail.com>
2023-10-28 06:19:08 +0100

    some changes done

--- app/components/atoms/coursedetailcompa.tsx
+++ app/components/atoms/coursedetailcompa.tsx
@@ -5,10 +5,12 @@ const Coursedetailcompa = ({ course }) => {
     <div>
       <div className="bg-black w-full h-[350px]">
         <div className=" flex">
-          <p>{course.category}</p>
-          <p>{course.subCategory}</p>
-          <p>{}</p>
+          <p className="text-violet">{course.category}</p>
+          <p className="text-violet">{course.subCategory}</p>
+          <p className="text-violet">{course.title}</p>
         </div>
+        <h1 className="text-[35px]">{course}</h1>
+        <p>{course.description}</p>
       </div>
     </div>
   );
