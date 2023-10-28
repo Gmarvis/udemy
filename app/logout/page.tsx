35e1109c1a5939b345badfe54c8e3337c3f8cc7b
frckbrice <maebrie2017@gmail.com>
2023-10-28 19:50:49 +0100

    fixing conflicts logout page

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -25,36 +25,33 @@ const Page = (props: Props) => {
   if (!hasmounted) return null;
 
   return (
-    <div className=" m-5 mt-3 ">
-      <div className=" bg-[#acd2cc] py-2 px-4  mb-6 mt-5 text-[#2d2f31] text-sm flex items-center gap-4 justify-start">
-        <div className=" m-5 ">
-          <MainNav />
-
-          <BsCheckCircleFill
-            className="bg-transparant text-[#2d2f31] text-xl mr-1 rounded-full border-0 mt-3 outline-none w-8 h-8"
-            // size={50}
-          />
-          {/* {loggedOut && ( */}
-          <h5> You’ve successfully logged out of Udemy. Come back soon!</h5>
-          {/* )}{" "} */}
-        </div>
-        <div className=" h-[350px] mb-3">
-          {" "}
-          <UdemyLogoutLogo img="https://s.udemycdn.com/logout/ufb-logout-hero-desktop-v2.jpg" />
-          <UdemyBusinessCard />
-        </div>
-
-        <div className="mb-5 ml-0 hidden sm:block sm:mt-0  md:mt-24">
-          <TabSliderLogout />
-        </div>
-        <div className="mb-5 ml-0 sm:hidden mt-44 sm:mt-0">
-          <ResponsiveTab />
-        </div>
-        <div>
-          <SliderComponent title="Learners are viewing" />
-        </div>
-        <Footer />
+    <div className=" m-5 ">
+      <MainNav />
+      <div className=" bg-[#acd2cc] py-2 px-4  my-[2.4rem] text-[#2d2f31] text-sm flex items-center gap-4 justify-start">
+        <BsCheckCircleFill
+          className="bg-transparant text-[#2d2f31] text-xl mr-1 rounded-full border-0 mt-3 outline-none w-8 h-8"
+          // saqize={50}
+        />
+        {/* {loggedOut && ( */}
+        <h5> You’ve successfully logged out of Udemy. Come back soon!</h5>
+        {/* )}{" "} */}
       </div>
+      <div className=" h-[350px] mb-3">
+        {" "}
+        <UdemyLogoutLogo img="https://s.udemycdn.com/logout/ufb-logout-hero-desktop-v2.jpg" />
+        <UdemyBusinessCard />
+      </div>
+
+      <div className="mb-5 ml-0 hidden sm:block sm:mt-0  md:mt-24">
+        <TabSliderLogout />
+      </div>
+      <div className="mb-5 ml-0 sm:hidden mt-44 sm:mt-0">
+        <ResponsiveTab />
+      </div>
+      <div>
+        <SliderComponent title="Learners are viewing" />
+      </div>
+      <Footer />
     </div>
   );
 };
