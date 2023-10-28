82765399971606f8f07090bb924e904288704d5e
GaelTikeng <Gaelinho@yahoo.fr>
2023-10-28 09:12:20 +0100

    first part of page ie payment method done

--- app/payment/checkout/page.tsx
+++ app/payment/checkout/page.tsx
@@ -8,6 +8,11 @@ type Props = {};
 
 const CheckoutPage = (props: Props) => {
   const [paypalActive, setPaypalActive] = React.useState(false);
+  const [cartActive, setCartActive] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [cartNumber, setCartNumber] = React.useState("");
+  const [expireDate, setExpireDate] = React.useState("");
+  const [cvc, setCvc] = React.useState("");
   const countries: { name: string; code: string }[] = [
     { name: "Afghanistan", code: "AF" },
     { name: "Åland Islands", code: "AX" },
@@ -262,6 +267,17 @@ const CheckoutPage = (props: Props) => {
     { name: "Zambia", code: "ZM" },
     { name: "Zimbabwe", code: "ZW" },
   ];
+
+  const handleCart = () => {
+    setCartActive((prev) => !prev);
+    setPaypalActive(false);
+  };
+
+  const handlePaypal = () => {
+    setPaypalActive((prev) => !prev);
+    setCartActive(false);
+  };
+
   function handleClick(): void {
     console.log("this is the function");
   }
@@ -290,7 +306,7 @@ const CheckoutPage = (props: Props) => {
           Cancel
         </p>
       </div>
-      <div className="flex md:px-4">
+      <div className="md:flex md:px-4 px-5">
         <div className="md:w-[55%] md:flex">
           <p className="flex-1"></p>
           <div className="md:pr-10 md:w-[60%]">
@@ -302,8 +318,8 @@ const CheckoutPage = (props: Props) => {
                 Billing address
               </h2>
             </div>
-            <div className=" w-1/2">
-              <div className="flex justify-between">
+            <div className="w-full md:w-1/2 py-3">
+              <div className="flex justify-between py-2">
                 <label className="font-bold">Country</label>
                 <span className="text-sm text-gray">Required</span>
               </div>
@@ -313,11 +329,11 @@ const CheckoutPage = (props: Props) => {
                 ))}
               </select>
             </div>
-            <span className="text-xs text-black">
+            <span className="text-xs text-black pt-4">
               Udemy is required by law to collect applicable transaction taxes
               for purchases made in certain tax jurisdictions.
             </span>
-            <section className="">
+            <section className="py-5">
               <div className="flex justify-between">
                 <h2 className="font-semibold text-xl md:text-2xl leading-normal">
                   Payment method
@@ -327,24 +343,142 @@ const CheckoutPage = (props: Props) => {
                   <IoMdLock />
                 </div>
               </div>
-              <div
-                className="hover:cursor-pointer border p-3 bg-gray flex gap-3 rounded"
-                onClick={() => setPaypalActive((prev) => !prev)}
-              >
-                <input
-                  type="radio"
-                  className="hover:cursor-pointer w-4"
-                  onChange={() => setPaypalActive((prev) => !prev)}
-                />
-                <div className="bg-white">
-                  <Image
-                    width={40}
-                    height={20}
-                    src="https://www.udemy.com/staticx/udemy/images/v9/hpp-paypal.svg"
-                    alt="paypal logo"
+              <div className="bg-gray border">
+                <div
+                  className="hover:cursor-pointer  p-3 flex gap-3 "
+                  onClick={() => handlePaypal()}
+                >
+                  <input
+                    type="radio"
+                    className="hover:cursor-pointer w-4"
+                    onChange={() => handleCart}
                   />
+                  <div className="bg-white rounded">
+                    <Image
+                      width={40}
+                      height={20}
+                      src="https://www.udemy.com/staticx/udemy/images/v9/hpp-paypal.svg"
+                      alt="paypal logo"
+                    />
+                  </div>
+                  <p className=" font-semibold">PayPal</p>
                 </div>
-                <p className=" font-semibold">PayPal</p>
+                <hr />
+                <p className={paypalActive ? "flex p-6 bg-white" : "hidden"}>
+                  In order to complete your transaction, we will transfer you
+                  over to PayPal's secure servers.
+                </p>
+                <div
+                  className="hover:cursor-pointer p-3"
+                  onClick={() => handleCart()}
+                >
+                  <div className="flex justify-between w-full">
+                    <div className="flex gap-3 flex-1 w-[60%] ">
+                      <input
+                        type="radio"
+                        className="hover:cursor-pointer w-4"
+                        onChange={() => setCartActive((prev) => !prev)}
+                      />
+                      <div className="bg-white h-fit my-auto rounded">
+                        <Image
+                          width={40}
+                          height={20}
+                          src="https://www.udemy.com/staticx/udemy/images/v9/card-default.svg"
+                          alt="cart deault logo"
+                        />
+                      </div>
+                      <p className="my-auto font-semibold flex-1">Credit/Debit Cart</p>
+                    </div>
+                    <div className="flex gap-2 flex-wrap w-[40%]  pl-8">
+                      <div className="bg-white rounded">
+                        <Image
+                          height={20}
+                          width={40}
+                          src="https://www.udemy.com/staticx/udemy/images/v9/card-amex.svg"
+                          alt="card-amex"
+                        />
+                      </div>
+                      <div className="bg-white rounded">
+                        <Image
+                          height={20}
+                          width={40}
+                          src="	https://www.udemy.com/staticx/udemy/images/v9/card-discover.svg"
+                          alt="card-amex"
+                        />
+                      </div>
+                      <div className="bg-white rounded">
+                        <Image
+                          height={20}
+                          width={40}
+                          src="https://www.udemy.com/staticx/udemy/images/v9/card-mastercard.svg"
+                          alt="card-mastercard"
+                        />
+                      </div>
+                      <div className="bg-white rounded">
+                        <Image
+                          height={20}
+                          width={40}
+                          src="	https://www.udemy.com/staticx/udemy/images/v9/card-visa.svg"
+                          alt="card-visa"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <hr />
+                <div className={cartActive ? "w-full px-5 py-5" : "hidden"}>
+                  <div className="flex justify-between py-2">
+                    <label className="font-bold">Name on cart</label>
+                    <span className="text-sm text-gray">Required</span>
+                  </div>
+                  <input
+                    className="border px-4 py-3 w-full"
+                    placeholder="name on cart"
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+
+                  <div className="flex justify-between py-2">
+                    <label className="font-bold">Cart number</label>
+                    <span className="text-sm text-gray">Required</span>
+                  </div>
+                  <input
+                    className="border px-4 py-3 w-full"
+                    placeholder="1234 5678 9012 3456"
+                    type="text"
+                    onChange={(e) => setCartNumber(e.target.value)}
+                  />
+                  <div className="flex gap-2 pb-5">
+                    <div className="w-1/2">
+                      <div className="flex justify-between py-2">
+                        <label className="font-bold">Expire date</label>
+                        <span className="text-sm text-gray">Required</span>
+                      </div>
+                      <input
+                        className="border px-4 py-3 w-full"
+                        placeholder="MM/YY"
+                        type="text"
+                        onChange={(e) => setExpireDate(e.target.value)}
+                      />
+                    </div>
+                    <div className="w-1/2">
+                      <div className="flex justify-between py-2">
+                        <label className="font-bold">CVC/CVV</label>
+                        <span className="text-sm text-gray">Required</span>
+                      </div>
+                      <input
+                        className="border px-4 py-3 w-full"
+                        type="text"
+                        placeholder="CVC"
+                        onChange={(e) => setCvc(e.target.value)}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex gap-2">
+                    <input type="checkbox" className="w-4 border" />
+                    <span>Securely save this cart for my later purchase</span>
+                  </div>
+                </div>
               </div>
             </section>
           </div>
