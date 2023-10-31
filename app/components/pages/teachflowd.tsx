f509254350c945ae6877ae8f804367aa0796e0f4
ricardo <ricardotsopse@gmail.com>
2023-10-31 05:25:08 +0100

    some slight changes done in the teachflow component

--- app/components/pages/teachflowd.tsx
+++ app/components/pages/teachflowd.tsx
@@ -2,15 +2,21 @@
 import Image from "next/image";
 import Button from "../atoms/Button";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Teachflowd = () => {
   const router = useRouter();
+  const [selectedRadioInput, setSelectedRadioInput] = useState("");
 
   const handleClickc = () => {
     router.push("/teachflowc");
   };
   const handleClickd = () => {
     router.push("/manage");
+  };
+
+  const handleChange = (e: any) => {
+    setSelectedRadioInput(e.target.value);
   };
 
   return (
@@ -47,21 +53,49 @@ const Teachflowd = () => {
           you don&apos;t have much time.
         </p>
         <div className="border border-black px-3 py-4 flex gap-4 w-1/3 hover:bg-gray">
-          <input className="rounded w-[15px] bg-black" type="radio" />
+          <input
+            className="rounded w-[15px] bg-black"
+            type="radio"
+            name="radio-input"
+            value="option-1"
+            onChange={handleChange}
+            checked={selectedRadioInput === "option-1"}
+          />
           <p className="font-bold">I&apos;m very busy right now (0-2hours)</p>
         </div>
         <div className="border border-black px-3 py-4 flex gap-4 w-1/3 hover:bg-gray">
-          <input className="rounded w-[15px]" type="radio" />
+          <input
+            className="rounded w-[15px]"
+            type="radio"
+            name="radio-input"
+            value="option-2"
+            onChange={handleChange}
+            checked={selectedRadioInput === "option-2"}
+          />
           <p className="font-bold">
             I&apos;ll work on this on the side (2-4hours)
           </p>
         </div>
         <div className="border border-black px-3 py-4 flex gap-4 w-1/3 hover:bg-gray">
-          <input className="rounded w-[15px]" type="radio" />
+          <input
+            className="rounded w-[15px]"
+            type="radio"
+            name="radio-input"
+            value="option-3"
+            onChange={handleChange}
+            checked={selectedRadioInput === "option-3"}
+          />
           <p className="font-bold">I have lots of flexibility (5+ hours)</p>
         </div>
         <div className="border border-black px-3 py-4 flex gap-4 w-1/3 hover:bg-gray">
-          <input className="rounded-full w-[15px]" type="radio" />
+          <input
+            className="rounded-full w-[15px]"
+            type="radio"
+            name="radio-input"
+            value="option-4"
+            onChange={handleChange}
+            checked={selectedRadioInput === "option-4"}
+          />
           <p className="font-bold">I haven&apos;t yet decided if I have time</p>
         </div>
       </div>
@@ -72,12 +106,14 @@ const Teachflowd = () => {
           className="border border-black hover:text-white hover:bg-black font-bold bg-white px-6 py-[12px] my-2"
           label="Previous"
           onClick={handleClickc}
+          style={""}
         />
         <Button
           type="submit"
           className="border border-black hover:text-white hover:bg-black font-bold bg-white hover: px-6 py-[12px] my-2"
           label="Create course"
           onClick={handleClickd}
+          style={""}
         />
       </div>
     </div>
